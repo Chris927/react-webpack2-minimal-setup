308a780db9d21f778fa0e4a5b5d3fcdf839aa43b
Chris Oloff <christian.oloff@gmail.com>
2017-03-15 16:12:49 +0200

    'npm run dev' should be working now

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ import { render } from 'react-dom'
 const App = () => (
   <div>
     <h1>Hello</h1>
-    <p>This is react. It is {new Date()}.</p>
+    <p>This is react, and webpack. It is {new Date().toString()}.</p>
   </div>
 )
 
-render(App, document.getElementById('app'))
+render(<App />, document.getElementById('app'))
